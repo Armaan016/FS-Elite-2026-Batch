1aea18766546009f11c6c69aa18f11d1e052e8a0
Armaan <armaanuddin0016@gmail.com>
2025-04-15 21:53:20 +0530

    Add Backtracking codes

---
+++ Day35/DistinctChunks.java
@@ -0,0 +1,85 @@
+// Captain Raynor is on a mission to decode an alien transmission.
+// The transmission contains a single long string S, and it is believed to be
+// made up of multiple unique signal chunks, sent one after another with no
+// spaces.
+
+// Your task is to help Captain Raynor split the transmission into the maximum
+// number of non-empty, unique signal chunks such that when all chunks are
+// concatenated in order, they exactly recreate the original transmission S.
+
+// Your goal is to maximize the number of unique chunks the message can be split
+// into.
+
+// Input Format:
+// Line-1: A string S representing the alien transmission.
+
+// Output Format:
+// Print a single integer – the maximum number of distinct chunks the string can
+// be split into.
+
+// Sample Input-1:
+// ---------------
+// banana
+
+// Sample Output-1:
+// ----------------
+// 4
+
+// Explanation:
+// ------------
+// One valid way to split the string is: "b", "a", "n", "ana".
+// This keeps all chunks unique.
+// Another way like "b", "a", "n", "an", "a" is invalid because "a" appears
+// twice.
+
+// Sample Input-2:
+// ---------------
+// mississippi
+
+// Sample Output-2:
+// ----------------
+// 7
+
+// Explanation:
+// ------------
+// One valid way to split it is: "m", "i", "s", "si", "ssi", "p", "pi".
+// All chunks are distinct and together recreate the original transmission.
+
+// NOTE: Only contiguous chunks (i.e., substrings) are allowed.
+// Subsequence-based
+// splitting is not permitted.
+
+import java.util.*;
+
+public class DistinctChunks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+        maxSize = Integer.MIN_VALUE;
+
+        backtrack(word, 0, new HashSet<>());
+        System.out.println(maxSize);
+
+        sc.close();
+    }
+
+    private static int maxSize;
+
+    private static void backtrack(String word, int idx, Set<String> set) {
+        if (idx == word.length()) {
+            // System.out.println("Set: " + set);
+            maxSize = Math.max(maxSize, set.size());
+            return;
+        }
+
+        for (int i = idx + 1; i <= word.length(); i++) {
+            String substr = word.substring(idx, i);
+            if (!set.contains(substr)) {
+                set.add(substr);
+                backtrack(word, i, set);
+                set.remove(substr);
+            }
+        }
+    }
+}
---
+++ Day35/MatchStrings.java
@@ -0,0 +1,93 @@
+// Given two strings S1 and S2, find if S2 can match S1 or not.
+
+// A match that is both one-to-one (an injection) and onto (a surjection),
+// i.e. a function which relates each letter in string S1 to a separate and
+// distinct non-empty substring in S2, where each non-empty substring in S2
+// also has a corresponding letter in S1.
+
+// Return true,if S2 can match S1.
+// Otherwise false.
+
+// Input Format:
+// -------------
+// Line-1 -> Two strings S1 and S2
+
+// Output Format:
+// --------------
+// Print a boolean value as result.
+
+// Sample Input-1:
+// ---------------
+// abab kmitngitkmitngit
+
+// Sample Output-1:
+// ----------------
+// true
+
+// Sample Input-2:
+// ---------------
+// aaaa kmjckmjckmjckmjc
+
+// Sample Output-2:
+// ----------------
+// true
+
+// Sample Input-3:
+// ---------------
+// mmnn pqrxyzpqrxyz
+
+// Sample Output-3:
+// ----------------
+// false
+
+import java.util.*;
+
+public class MatchStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] line = sc.nextLine().split(" ");
+        String s1 = line[0];
+        String s2 = line[1];
+
+        System.out.println(canMatch(s1, s2, 0, 0, new HashMap<>(), new HashSet<>()));
+
+        sc.close();
+    }
+
+    private static boolean canMatch(String s1, String s2, int i, int j, Map<Character, String> map, Set<String> used) {
+        if (i == s1.length() && j == s2.length()) {
+            // System.out.println("map: " + map);
+            return true;
+        }
+        if (i == s1.length() || j == s2.length())
+            return false;
+
+        char a_ = s1.charAt(i);
+        for (int idx = j + 1; idx <= s2.length(); idx++) {
+            String b_ = s2.substring(j, idx);
+            // System.out.printf("a_: %c, b_: %s\n", a_, b_);
+
+            if (map.containsKey(a_)) {
+                if (!map.get(a_).equals(b_))
+                    continue;
+                if (canMatch(s1, s2, i + 1, idx, map, used))
+                    return true;
+            } else {
+                if (used.contains(b_))
+                    continue;
+
+                map.put(a_, b_);
+                used.add(b_);
+                // System.out.println("map: " + map);
+                if (canMatch(s1, s2, i + 1, idx, map, used))
+                    return true;
+
+                map.remove(a_);
+                used.remove(b_);
+            }
+        }
+
+        return false;
+    }
+}
